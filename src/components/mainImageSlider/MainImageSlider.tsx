9d9dffa0128db047e825499b9226bb29b620acef
Vladislav Shitov <sayroxskrim@bk.ru>
2025-12-04 23:17:16 +0400

    доделал слайдер на главной странице (не сделано только чтобы при нажатии открывалось на весь экран)

--- src/components/mainImageSlider/MainImageSlider.tsx
+++ src/components/mainImageSlider/MainImageSlider.tsx
@@ -7,10 +7,11 @@ const MainImageSlider = () => {
   const [typeSelector, setTypeSelector] = useState("Museum");
   const [currentSlide, setCurrentSlide] = useState(0);
   const [touchStart, setTouchStart] = useState(0);
-  const baseImage = data?.mainScreenMuseumImages[0] || {
-    file: "path/to/image.jpg",
-  };
-  const fakeImages = Array(5).fill(baseImage);
+
+  const images =
+    typeSelector === "Museum"
+      ? (data?.mainScreenMuseumImages ?? [])
+      : (data?.mainScreenLiceyImages ?? []);
 
   const handleTouchStart = (e: React.TouchEvent) => {
     setTouchStart(e.touches[0].clientX);
@@ -21,15 +22,15 @@ const MainImageSlider = () => {
     const touchEnd = e.touches[0].clientX;
     if (touchStart - touchEnd > 600) {
       setCurrentSlide((prev) =>
-        currentSlide !== fakeImages.length - 1
-          ? Math.min(prev + 1, fakeImages.length - 1)
+        currentSlide !== images.length - 1
+          ? Math.min(prev + 1, images?.length - 1)
           : 0,
       );
       setTouchStart(0);
     }
     if (touchStart - touchEnd < -600) {
       setCurrentSlide((prev) =>
-        currentSlide !== 0 ? Math.max(prev - 1, 0) : fakeImages.length - 1,
+        currentSlide !== 0 ? Math.max(prev - 1, 0) : images.length - 1,
       );
       setTouchStart(0);
     }
@@ -37,52 +38,56 @@ const MainImageSlider = () => {
 
   return (
     <>
-      <div className={styles.imageSlider}>
-        <div className={styles.typeSelector}>
-          <button
-            onClick={() => setTypeSelector("Museum")}
-            className={typeSelector === "Museum" ? styles.active : ""}
-          >
-            <p>Музей</p>
-          </button>
-          <button
-            onClick={() => setTypeSelector("Licey")}
-            className={typeSelector === "Licey" ? styles.active : ""}
-          >
-            <p>Лицей</p>
-          </button>
-        </div>
-        <div
-          className={styles.imageContainer}
-          onTouchMove={handleTouchMove}
-          onTouchStart={handleTouchStart}
-        >
-          <div
-            className={styles.slidesWrapper}
-            style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-          >
-            {fakeImages.map((img, i) => (
-              <img
-                key={i}
-                src={`${API_BASE_URL}${img.file}`}
-                alt=""
-                className={styles.slide}
-              />
-            ))}
+      {images.length && (
+        <div className={styles.imageSlider}>
+          <div className={styles.typeSelector}>
+            <button
+              onClick={() => setTypeSelector("Museum")}
+              className={typeSelector === "Museum" ? styles.active : ""}
+            >
+              <p>Музей</p>
+            </button>
+            <button
+              onClick={() => setTypeSelector("Licey")}
+              className={typeSelector === "Licey" ? styles.active : ""}
+            >
+              <p>Лицей</p>
+            </button>
           </div>
+          <div
+            className={styles.imageContainer}
+            onTouchMove={handleTouchMove}
+            onTouchStart={handleTouchStart}
+          >
+            <div
+              className={styles.slidesWrapper}
+              style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+            >
+              {images.map((img, i) => (
+                <img
+                  key={i}
+                  src={`${API_BASE_URL}${img.file}`}
+                  alt=""
+                  className={styles.slide}
+                />
+              ))}
+            </div>
+          </div>
+          {images.length > 1 && (
+            <div className={styles.sliderFrameList}>
+              {images.map((_, i) => {
+                return (
+                  <div
+                    className={`${styles.circle} ${currentSlide === i ? styles.activeCircle : ""}`}
+                    key={i}
+                    onClick={() => setCurrentSlide(i)}
+                  ></div>
+                );
+              })}
+            </div>
+          )}
         </div>
-        <div className={styles.sliderFrameList}>
-          {fakeImages.map((_, i) => {
-            return (
-              <div
-                className={`${styles.circle} ${currentSlide === i ? styles.activeCircle : ""}`}
-                key={i}
-                onClick={() => setCurrentSlide(i)}
-              ></div>
-            );
-          })}
-        </div>
-      </div>
+      )}
     </>
   );
 };
