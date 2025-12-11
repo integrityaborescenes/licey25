80ced8510fe2e25196b48b3a20f9bf077e425ba3
Vladislav Shitov <sayroxskrim@bk.ru>
2025-12-11 14:52:28 +0400

    добавил lazy loading для HistoryMainBlock

--- src/components/HistoryMainBlock/HistoryMainBlock.tsx
+++ src/components/HistoryMainBlock/HistoryMainBlock.tsx
@@ -3,16 +3,34 @@ import { useGetHistoryQuery } from "../../store/services/history.api.ts";
 import { Link } from "react-router";
 import { useGetPersonDataQuery } from "../../store/services/personData.api.ts";
 import { API_URL } from "../../config.ts";
+import { useMemo, useState } from "react";
 
 const HistoryMainBlock = () => {
   const { data } = useGetHistoryQuery();
   const { data: person } = useGetPersonDataQuery();
   const historyText = data?.text;
+
+  const [visibleCount, setVisibleCount] = useState(6);
+  const filteredData = useMemo(() => {
+    return person?.slice(0, visibleCount) || [];
+  }, [person, visibleCount]);
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+
+    if (scrollHeight - (scrollTop + clientHeight) < 500) {
+      setVisibleCount((prev) => Math.min(prev + 6, person?.length || 0));
+    }
+  };
   return (
     <div className={styles.historyBlock}>
       <div className={styles.photos}>
-        <div className={styles.photosContainer} data-scroll-id={"history"}>
-          {person?.map((p) => {
+        <div
+          className={styles.photosContainer}
+          data-scroll-id={"history"}
+          onScroll={handleScroll}
+        >
+          {filteredData.map((p) => {
             return (
               <div className={styles.item} key={p.id}>
                 <Link to={`/history/${p.id}`} state={p}>
